5d6d5574e3886f6b2b0a2fa8751358cdfddfcd9d
ASUS <eltac.shixseyidov250301@gmail.com>
2020-04-10 00:28:28 +0400

    first message

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -4,7 +4,7 @@ import java.net.*;
 import  java.io.*;
 
 public class Client {
-
+// h
     private Client(String address, int port){
         Socket socket = null;
         DataInputStream dataInputStream = null;
